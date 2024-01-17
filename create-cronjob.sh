7cbd7bcd2b32cbd3d1a1bc1e8508a739f2f15e89
Frank Martin <frank@grafikus.nl>
2024-01-17 13:59:43 +0100

    Removed prompt for install crontabs

--- create-cronjob.sh
+++ create-cronjob.sh
@@ -7,17 +7,12 @@ source $SCRIPT_DIR/utils.sh
 # Add a cron job that runs the start.sh script at startup
 print_step "Adding cron job"
 if ! check_command "crontab"; then
-    if confirm "Crontab is not installed. Do you want to install it?"; then
-        sudo dnf install crontabs -y
-    else
-        print_error "Crontab is required to add a cron job."
-        print_error "No cron job added"
-    fi
+    print_warning "Crontab not installed. Installing crontab."
+    sudo dnf install crontabs -y &>> $LOG_DIR/install-crontab.log
+fi
+if ! (crontab -l 2>/dev/null | grep -q "$SCRIPT_DIR/start.sh"); then
+    (crontab -l 2>/dev/null; echo "@reboot $SCRIPT_DIR/start.sh") | crontab -
+    print_step "Succesfully added cron job."
 else
-    if ! (crontab -l 2>/dev/null | grep -q "$SCRIPT_DIR/start.sh"); then
-        (crontab -l 2>/dev/null; echo "@reboot $SCRIPT_DIR/start.sh") | crontab -
-        print_step "Succesfully added cron job."
-    else
-        print_warning "Cron job already exists."
-    fi
+    print_warning "Cron job already exists."
 fi
